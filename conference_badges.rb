140266acbad0e488084add689782251925504cd6
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-16 02:30:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,13 @@
 def badge_maker (name)
-    puts "Hello, my name is #{name}"
+    p "Hello, my name is #{name}"
+end 
+
+def batch_badge_creator (attendees)
+  batch_badges=[]
+  i=0
+  attendees.each do |badge|
+    batch_badges[i] = "Hello, my name is #{badge}"
+    i+=
+  end 
+  p batch_badges
 end 
